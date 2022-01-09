43ae5fe7fcdb8d767c59fd899a426e6ee0ffe4ea
mohamedjamai <90444195+mohamedjamai@users.noreply.github.com>
2022-01-09 18:20:34 +0100

    Create Loops-For.php

---
+++ 2-Loops/taak02/Loops-For.php
@@ -0,0 +1,10 @@
+O index.php X
+2-Loops > taako1 >O index.php >..
+    <?php
+        for ($leeftijd = 0; $leeftijd <= 18; $leeftijd++){
+            echo "En dat is: $leeftijd <br/>";
+        }
+
+    print "Ik ben 18 jaar oud, ik heb stemrecht!";
+
+    ?>
