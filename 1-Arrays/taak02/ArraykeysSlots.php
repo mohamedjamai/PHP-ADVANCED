efb3e5770c735f3be7db3e70495ab9d513ac3813
mohamedjamai <90444195+mohamedjamai@users.noreply.github.com>
2022-01-09 17:47:23 +0100

    ARRAYSKEYS

---
+++ 1-Arrays/taak02/ArraykeysSlots.php
@@ -0,0 +1,11 @@
+<?php
+      $persoon["voornaam"] = "Willem";
+      $persoon["last"] = "Alexander";
+      $persoon["leeftijd"] = "53";
+      $persoon["opleiding"] = "Software Ontwikkeling";
+
+?>
+
+<?php
+echo  "$voornaam['voornaam'] $achternaam['last'] van Oranje is $leeftijd['leeftijd'] is Student aan het ROC van Amsterdam aan de opleiding $opleiding['opleiding']";
+?>
