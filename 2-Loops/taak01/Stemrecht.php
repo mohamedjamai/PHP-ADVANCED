849b6515a1430cd12560b0ad0a786e45a7c58d42
mohamedjamai <90444195+mohamedjamai@users.noreply.github.com>
2022-01-09 18:11:22 +0100

    STEMRECHT

---
+++ 2-Loops/taak01/Stemrecht.php
@@ -0,0 +1,13 @@
+O index.php X
+2-Loops > taako1 >O index.php >..
+    <?php
+    $leeftijd = 0;
+
+    while($leeftijd < 18){
+            echo "$leeftijd jaar oud, heb nog geen stemrecht <br>" ;
+            $leeftijd++;
+    }
+
+    print "Ik ben 18 jaar oud, ik heb stemrecht!";
+
+    ?>
