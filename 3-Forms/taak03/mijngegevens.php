9d67a1e2c63b12ebe6d97c8d0d224670cbbed7ef
mohamedjamai <90444195+mohamedjamai@users.noreply.github.com>
2022-01-29 02:42:51 +0100

    FORMS GET

---
+++ 3-Forms/taak03/mijngegevens.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+
+<?php
+
+
+
+ $_GET['artiest'] .  $_GET['titel'] . $_GET['JaarVanUitkomst'] . $_GET['muzieksoort'];
+?>
+
+
+<ol>
+  <li><?php echo $_GET['artiest'] ?></li>
+  <li><?php echo $_GET['titel']?></li>
+  <li><?php echo $_GET['JaarVanUitkomst']?></li>
+  <li><?php echo $_GET['muzieksoort']?></li>
+</ol>
+
+</body>
+</html>
---
+++ 3-Forms/taak03/mijnmuziek.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+
+<form name id="muziek" action = "mijngegevens.php" method="get">
+
+
+<a href="mijngegevens.php?
+artiest=travis scott&
+titel=sicko mode&
+JaarVanUitkomst=2017&
+muzieksoort=Rap">Test $GET</a>
+
+</body>
+</html>
