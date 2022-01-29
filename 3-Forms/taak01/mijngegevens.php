40bfe07831044c2c2a2ef08a67b0756b7f2f113a
mohamedjamai <90444195+mohamedjamai@users.noreply.github.com>
2022-01-29 02:38:15 +0100

    FORMS

---
+++ 3-Forms/taak01/mijngegevens.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+</head>
+<body>
+    
+<?php
+
+echo $_POST["fname"] , "<br>"; 
+
+echo $_POST["achternaam"] , "<br>"; 
+
+echo $_POST["klas"] , "<br>"; 
+
+echo $_POST["leeftijd"] , "<br>"; 
+
+echo $_POST["adres"] , "<br>"; 
+
+echo $_POST["woonplaats"] , "<br>"; 
+
+
+?>
+
+</body>
+</html>
