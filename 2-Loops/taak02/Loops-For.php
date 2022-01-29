14fbeecb95050256de5a2a965edb90499d487bff
mohamedjamai <90444195+mohamedjamai@users.noreply.github.com>
2022-01-29 02:18:33 +0100

    LoopsFor

--- 2-Loops/taak02/Loops-For.php
+++ 2-Loops/taak02/Loops-For.php
@@ -1,5 +1,13 @@
-O index.php X
-2-Loops > taako1 >O index.php >..
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
     <?php
         for ($leeftijd = 0; $leeftijd <= 18; $leeftijd++){
             echo "En dat is: $leeftijd <br/>";
@@ -8,3 +16,9 @@ O index.php X
     print "Ik ben 18 jaar oud, ik heb stemrecht!";
 
     ?>
+    </ul>
+</body>
+</html>
+
+
+    
