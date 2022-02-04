95e66408451db2c5ae103ad2fb22a3fcdf5ea32e
mohamedjamai <90444195+mohamedjamai@users.noreply.github.com>
2022-02-04 15:16:47 +0100

    .

--- 3-Forms/taak02/mijngegevens.php
+++ 3-Forms/taak02/mijngegevens.php
@@ -5,26 +5,7 @@
 <body>
     <h1>Jouw gegevens!</h1>
         <?php
-        
-        switch ($_POST["colors"]) {
-          case "4":
-            echo '<body style="background-color: yellow;">';
-            break;
-          case "2":
-              echo '<body style="background-color: red;">';
-            break;
-          case "3":
-              echo '<body style="background-color: blue;">';
-            break;
-            case "1":
-              echo '<body style="background-color: green;">';
-            break;
-            case "5":
-              echo '<body>';
-            break;
-          default:
-            echo "<body>";
-        }
+
 
         echo  "Voornaam is ". $_POST["fname"] ,"<br>"; 
         echo "Achteraam is ". $_POST["achternaam"] , "<br>"; 
