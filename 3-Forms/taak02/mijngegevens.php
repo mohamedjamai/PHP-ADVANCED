61ceb7b3db85ed14da6d2da6597cc48ad1e64f3b
mohamedjamai <90444195+mohamedjamai@users.noreply.github.com>
2022-02-04 15:15:52 +0100

    .
    Verbeterd 4-02

--- 3-Forms/taak02/mijngegevens.php
+++ 3-Forms/taak02/mijngegevens.php
@@ -1,40 +1,47 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+    <h1>Jouw gegevens!</h1>
+        <?php
+        
+        switch ($_POST["colors"]) {
+          case "4":
+            echo '<body style="background-color: yellow;">';
+            break;
+          case "2":
+              echo '<body style="background-color: red;">';
+            break;
+          case "3":
+              echo '<body style="background-color: blue;">';
+            break;
+            case "1":
+              echo '<body style="background-color: green;">';
+            break;
+            case "5":
+              echo '<body>';
+            break;
+          default:
+            echo "<body>";
+        }
 
-switch ($_POST["colors"]) {
-    case "yellow":
-      echo '<body style="background-color: yellow;">';
-      break;
-    case "red":
-        echo '<body style="background-color: red;">';
-      break;
-    case "blue":
-        echo '<body style="background-color: blue;">';
-      break;
-      case "green":
-        echo '<body style="background-color: green;">';
-      break;
-      case "white":
-        echo '<body>';
-      break;
-    default:
-      echo "<body>";
-  }
-
-echo $_POST["fname"] , "<br>"; 
-
-echo $_POST["achternaam"] , "<br>"; 
-
-echo $_POST["klas"] , "<br>"; 
-
-echo $_POST["leeftijd"] , "<br>"; 
-
-echo $_POST["adres"] , "<br>"; 
-
-echo $_POST["woonplaats"] , "<br>"; 
-
-echo $_POST["gender"] , "<br>";
+        echo  "Voornaam is ". $_POST["fname"] ,"<br>"; 
+        echo "Achteraam is ". $_POST["achternaam"] , "<br>"; 
+        echo "Jouw klas is ". $_POST["klas"] , "<br>"; 
+        echo "Jouw leeftijd is ". $_POST["leeftijd"] , "<br>"; 
+        echo "Jouw adress is ". $_POST["adres"] , "<br>"; 
+        echo "Jouw woonplaats is ". $_POST["woonplaats"] , "<br>"; 
+        echo "Jouw lievelings muziekband is ". $_POST["Muziekband"] , "<br>"; 
+        echo "Jouw geslacht is ". $_POST["sex"] , "<br>"; 
+        
+        ?>
+        
 
 
 
+</body>
+</html>
 
-?>
+
+
