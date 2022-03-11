b37dafe78797c643d2f1730485cc98fd7242f1ee
mohamedjamai <90444195+mohamedjamai@users.noreply.github.com>
2022-03-11 14:30:07 +0100

    update fix

--- opdracht/advanced-2.php
+++ opdracht/advanced-2.php
@@ -1,24 +1,29 @@
+<body style="background-color:<?php echo $_POST['achtergrondkleur']; ?>; border-radius:<?php echo $_POST['borderline']; ?>; color:<?php echo $_POST['tesktkleur1']; ?>">
+<style>
+table, th, td {
+  border: 1px solid white;
+}
+    </style>
+    <?php
+    $persoonlijkeArray = array(
+        "Naam" => "Mohamed",
+        "achternaam" => "Jamai",
+        "Leeftijd" => "17",
+        "Woonplaats" => "Amsterdam",
+        "Geboortedatum" => "12 Oktober 2004",
+    );
+    echo "<table>";
+    foreach ($persoonlijkeArray as $key => $value) {
+        maakRij($key, $value);
+    }
+    echo "</table>";
 
+    function maakRij($key, $value)
+    {
+        echo "<tr>";
+        echo "<td> $key </td>";
+        echo "<td> $value </td>";
+        echo "</tr>";
+    }
 
-<body style="background-color:<?php echo $_POST['achtergrondkleur']; ?>; border-radius:<?php echo $_POST['borderline']; ?>; color:<?php echo $_POST['tesktkleur1'];?>">
-
-<?php
-                            $persoonlijkeArray = array(
-                            "Naam" => "Mohamed",
-                            "achternaam" => "Jamai",
-                            "Leeftijd" => "17",
-                            "Woonplaats" => "Amsterdam",
-                            "Geboortedatum" => "12 Oktober 2004",
-
-                        );
-                        
-                        foreach ($persoonlijkeArray as $key => $value) {
-                            echo "<tr>";
-                            echo "<td> $key </td>";
-                            echo "<td> $value </td>";
-                            echo "</tr>";
-                        };
-                      
-                        
-                            ?>
-                    
+    ?>
